775b0bd94dead42fc28ea806d2d8b8d927c02533
philManWithBeard <48831325+philManWithBeard@users.noreply.github.com>
2020-05-22 14:04:25 +0100

    Removed colon from question number

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ function displayIntro() {
 function displayQuestions() {
   $('.quizIntro').hide()
   $('.scoreBox').show()
-  $('.quizTitle').html(`Question: ${questionNumber + 1}`)
+  $('.quizTitle').html(`Question ${questionNumber + 1}`)
   $('.questionNumber').hide()
   $('.score').html(`${score} / ${quiz.quizContent.questions.length}`)
   $('.question').html(quiz.quizContent.questions[questionNumber].question)
